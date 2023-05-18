770c22c47bb72ebfc39cfab2b83fc89ebb6ead69
Nivin Vysakh <nivinsvysakh2023@gmail.com>
2023-05-18 08:19:34 +0530

    Create Arraysloop.c

---
+++ Arraysloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int numb[] = {1 , 2 , 3 , 4};
+int i = 0;
+
+int main(){
+
+    for (i = 0 ; i <4 ; i++) {
+        printf("%d" , numb[i]);
+    }
+
+    
+
+    return 0 ;
+}
